ad87bd535dd97a230db116651c1d389fe6b84083
markthree <1801982702@qq.com>
2024-02-06 15:34:46 +0800

    refactor: only deno deploy use spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 import nitroPort from "nitro-port-module";
 
+const isDenoDeployPreset = process.env.NITRO_PRESET === "deno-deploy";
+
 export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-  ssr: false,
+  ssr: !isDenoDeployPreset,
   modules: ["@nuxt/ui"],
   nitro: {
     modules: [
